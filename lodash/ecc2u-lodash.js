06e3c7df1ddc2e2a0616fff7dfba73ed321e3399
AlviN <doicic409393@gmail.com>
2022-09-14 18:46:27 +0800

    ccc

--- lodash/ecc2u-lodash.js
+++ lodash/ecc2u-lodash.js
@@ -1,3 +1,69 @@
+//lodash 实践
+
 var ecc2u = {
-  
+  chunk: function (array, size) {
+
+  },
+  compact: function (array) {
+    var res = []
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] !== false && array[i] !== 0 && array[i] !== null && array[i] !== "") {
+        res.push(array[i])
+      }
+    }
+    return res
+  },
+  difference: function (array, array2) {
+    var res = [];
+    for (let index = 0; index < array.length; index++) {
+      const element = array[index];
+      if (element !== array2[index]) {
+        res.push(element)
+      }
+    }
+    return res
+  },
+  differenceBy: function () {
+    var res = [];
+
+    return res
+  },
+  differenceWith: function () {
+
+  },
+  drop: function () {
+
+  },
+  dropRight: function () {
+
+  },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
